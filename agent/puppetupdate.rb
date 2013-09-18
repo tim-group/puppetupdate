f531405d16ab9f4d19cee484b2fdba40ed0d0b2d
Maksym Melnychok <maksym.melnychok@gmail.com>
2013-09-18 12:38:59 +0200

    do/end multiline blocks, drop few returns

--- agent/puppetupdate.rb
+++ agent/puppetupdate.rb
@@ -59,11 +59,11 @@ module MCollective
       def update_all_branches(revisions={})
         update_bare_repo
         branches = branches
-        branches.each { |branch|
+        branches.each do |branch|
           debug "WORKING FOR BRANCH #{branch}"
           debug "#{revisions[branch]}"
           update_branch(branch, revisions[branch])
-        }
+        end
         write_puppet_conf(branches)
         cleanup_old_branches(branches)
       end
@@ -72,25 +72,24 @@ module MCollective
         local_branches = ["default"]
         branches.each { |branch| local_branches << local_branch_name(branch) }
         all_envs = all_env_branches()
-        all_envs.each { |branch|
-          unless local_branches.include?(branch)
-            debug "Cleanup old branch named #{branch}"
-            exec "rm -rf #{@dir}/environments/#{branch}"
-          end
-        }
+        all_envs.each do |branch|
+          next if local_branches.include?(branch)
+
+          debug "Cleanup old branch named #{branch}"
+          exec "rm -rf #{@dir}/environments/#{branch}"
+        end
       end
 
       def write_puppet_conf(branches)
         branches << "default"
         FileUtils.cp "#{@dir}/puppet.conf.base", "#{@dir}/puppet.conf"
-        branches.each {
-          |branch|
-          open("#{@dir}/puppet.conf", "a") {|f|
+        branches.each do |branch|
+          open("#{@dir}/puppet.conf", "a") do |f|
             f.puts "\n[#{local_branch_name(branch)}]\n"
             f.puts "modulepath=$confdir/environments/#{local_branch_name(branch)}/modules\n"
             f.puts "manifest=$confdir/environments/#{local_branch_name(branch)}/manifests/site.pp\n"
-          }
-        }
+          end
+        end
       end
 
       def branches
@@ -103,7 +102,7 @@ module MCollective
             end
           end
         end
-        return branches
+        branches
       end
 
       def all_env_branches
@@ -114,7 +113,7 @@ module MCollective
             branches<<line
           end
         end
-        return branches
+        branches
       end
 
       def update_branch(remote_branch_name, revision=nil)
@@ -132,20 +131,15 @@ module MCollective
       end
 
       def remote_branch_name(remote_branch_name)
-        if /\* (.+)/.match(remote_branch_name)
-          return $1
-        end
-        return remote_branch_name
+        /\* (.+)/.match(remote_branch_name) ? $1 : remote_branch_name
       end
 
       def local_branch_name(remote_branch_name)
         if /(\/|\* )(.+)/.match(remote_branch_name)
           remote_branch_name = $2
         end
-        if remote_branch_name == 'master'
-          return "masterbranch"
-        end
-        return remote_branch_name
+
+        remote_branch_name == 'master' ? "masterbranch" : remote_branch_name
       end
 
       def update_bare_repo
