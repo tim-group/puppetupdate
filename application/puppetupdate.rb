4e0a78209758ad82f33917a0efe5c1d4dd1c8cdd
Maksym Melnychok <maksym.melnychok@gmail.com>
2013-09-19 18:11:02 +0200

    accept branch param in application/puppetupdate.rb

--- application/puppetupdate.rb
+++ application/puppetupdate.rb
@@ -1,31 +1,26 @@
-class MCollective::Application::Puppetupdate<MCollective::Application
+class MCollective::Application::Puppetupdate < MCollective::Application
   description "Puppet repository updates Client"
-  usage "Usage: mco puppetupdate -T oyldn or mco puppetupdate <sha1>"
+  usage "Usage: mco puppetupdate -T oyldn or mco puppetupdate [<branch> [<sha1>]]"
 
   def post_option_parser(configuration)
     if ARGV.length >= 1
-      configuration[:command] = "update_default"
+      configuration[:command]  = "update"
+      configuration[:branch]   = ARGV.shift
       configuration[:revision] = ARGV.shift
     else
-      configuration[:command] = "update"
+      configuration[:command] = "update_all"
     end
   end
 
   def main
+    return unless %w{update_all update}.include? configuration[:command]
 
-    case configuration[:command]
-    when "update"
-      mc = rpcclient("puppetupdate", :options => options)
-      printrpc mc.update()
-      mc.disconnect
-      printrpcstats
-    when "update_default"
-      mc = rpcclient("puppetupdate", :options => options)
-      printrpc mc.update_default(:revision=>configuration[:revision])
-      mc.disconnect
-      printrpcstats
-    end
-
+    mc = rpcclient("puppetupdate", :options => options)
+    printrpc(
+      mc.send(configuration[:command],
+        :revision => configuration[:revision],
+        :branch   => configuration[:branch]))
+    mc.disconnect
+    printrpcstats
   end
 end
-
